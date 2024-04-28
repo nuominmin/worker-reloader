7753ef6397dd37aa917f31d36b9641922a54c259
NuoMinMin <776191753@qq.com>
2024-04-28 11:24:42 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nuominmin/workerreloader
+
+go 1.22.2
---
+++ worker.go
@@ -0,0 +1,101 @@
+package workerreloader
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+// WorkerManager defines the interface for managing worker tasks.
+type WorkerManager interface {
+	Start(name string, handle func(), interval time.Duration)
+	Stop(name string)
+	StopAll()
+}
+
+// WorkerPool manages a collection of worker tasks.
+type WorkerPool struct {
+	workers map[string]*WorkerTask
+}
+
+// WorkerTask represents a single scheduled task.
+type WorkerTask struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+	wg     sync.WaitGroup
+	mutex  sync.Mutex
+}
+
+// NewWorkerPool creates a new instance of WorkerPool.
+func NewWorkerPool() WorkerManager {
+	return &WorkerPool{
+		workers: make(map[string]*WorkerTask),
+	}
+}
+
+// Start initiates a worker task with the specified name, function, and interval. If a task with the same name exists, it restarts it.
+func (wp *WorkerPool) Start(name string, handle func(), interval time.Duration) {
+	if _, ok := wp.workers[name]; !ok {
+		wp.workers[name] = new(WorkerTask)
+		wp.workers[name].ctx, wp.workers[name].cancel = context.WithCancel(context.Background())
+	}
+
+	wp.workers[name].Start(handle, interval)
+}
+
+// Stop terminates the worker task specified by name.
+func (wp *WorkerPool) Stop(name string) {
+	if worker, ok := wp.workers[name]; ok {
+		worker.Stop()
+	}
+}
+
+// StopAll terminates all worker tasks managed by the pool.
+func (wp *WorkerPool) StopAll() {
+	for _, worker := range wp.workers {
+		worker.Stop()
+	}
+}
+
+// Start begins a new work routine and cancels any existing one.
+func (wt *WorkerTask) Start(handle func(), interval time.Duration) {
+	wt.mutex.Lock()
+	defer wt.mutex.Unlock()
+
+	// Cancel any existing work routine.
+	if wt.cancel != nil {
+		wt.cancel()
+		wt.wg.Wait() // Wait for the existing routine to stop
+	}
+
+	// Create a new context for the new work routine
+	wt.ctx, wt.cancel = context.WithCancel(context.Background())
+
+	// Start the new work routine
+	wt.wg.Add(1)
+	go func() {
+		defer wt.wg.Done()
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-wt.ctx.Done():
+				fmt.Println("Worker task stopped")
+				return
+			case <-ticker.C:
+				handle()
+			}
+		}
+	}()
+}
+
+// Stop cancels the work routine.
+func (wt *WorkerTask) Stop() {
+	wt.mutex.Lock()
+	defer wt.mutex.Unlock()
+	if wt.cancel != nil {
+		wt.cancel()
+		wt.wg.Wait() // Ensure the routine has stopped
+	}
+}
